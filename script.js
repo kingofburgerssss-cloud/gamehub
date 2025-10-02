fb5a580b7ea192c3af358fe8f217710c02185a85
kingofburgerssss-cloud <kingofburgerssss@gmail.com>
2025-10-01 20:52:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ class Particle {
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
     this.size = Math.random() * 3 + 1;
-    this.speedX = (Math.random() - 0.5) * 0.5;
-    this.speedY = (Math.random() - 0.5) * 0.5;
+    this.speedX = (Math.random() - 0.5) * 0.3;
+    this.speedY = (Math.random() - 0.5) * 0.3;
   }
   update() {
     this.x += this.speedX;
@@ -26,7 +26,7 @@ class Particle {
     if(this.y > canvas.height) this.y = 0;
   }
   draw() {
-    ctx.fillStyle = 'rgba(255,255,255,0.5)';
+    ctx.fillStyle = 'rgba(255,255,255,0.3)';
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI*2);
     ctx.fill();
@@ -37,10 +37,10 @@ function initParticles(num) {
   particles = [];
   for(let i=0;i<num;i++) particles.push(new Particle());
 }
-initParticles(100);
+initParticles(80);
 
 function animate() {
-  ctx.clearRect(0,0,canvas.width,canvas.height); // clear background only
+  ctx.clearRect(0,0,canvas.width,canvas.height);
   particles.forEach(p => { p.update(); p.draw(); });
   requestAnimationFrame(animate);
 }
